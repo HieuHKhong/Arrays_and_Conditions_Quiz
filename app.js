1af50142b5da5f0221fe2fef3b6eee396da24407
HieuHKhong <hieu.khong@mscoding.org>
2024-08-02 11:51:12 -0500

    Did the quiz.

---
+++ app.js
@@ -0,0 +1,35 @@
+// Question 1
+let fruits = ["apple", "banana", "cherry"];
+console.log(fruits[1]);
+
+// Question 2
+let num = [1,2,3,4,5];
+console.log(num[0] + num[2]);
+
+// Question 3
+let languages = ["Javascript", "Python", "Java"];
+languages[1] = "Ruby";
+console.log(languages);
+
+// Question 4
+let age = 20;
+
+if (age >= 18) {
+    console.log("Eligible to vote.")
+}else {
+    console.log("Not eligible to vote.")
+}
+
+// Question 5
+let cities = ["New York", "Los Angeles", "Chicago"];
+lastCity = cities[2];
+console.log(lastCity);
+
+// Question 6
+let x = 7;
+
+if (x > 5 || x < 10) {
+    console.log("In range.")
+}else {
+    "Out of range."
+}
